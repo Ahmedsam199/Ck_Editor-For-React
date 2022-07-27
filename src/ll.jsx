e8441942240b049c879f9573a36a25c22f311533
ahmed <@hotcav2@gmail.com>
2022-07-27 17:15:36 +0300

    initial commit

--- src/ll.jsx
+++
@@ -1,7 +0,0 @@
-const Hola = () => {
-    return ( <div className="Test">
-        wwswad
-    </div> );
-}
- 
-export default Hola;
--- src/sign.js
+++ src/sign.js
@@ -1,15 +1,32 @@
 import { Ref,useRef,useState } from "react";
+import { useSelector,useDispatch } from "react-redux";
+import {
+  BrowserRouter as Router,
+  Link,
+  Route,
+  Switch,
+  useHistory,
+} from "react-router-dom";
+
 const Sign = ({setName}) => {
+  const dispatch=useDispatch()
     const val1=useRef()
     const Add=()=>{
-          let data=val1.current.value
-            setName(data)
-console.log(data);
+      const newData = token2.Data.replace(">@Name<",">"+val1.current.value+"<");
+      
+console.log(newData)
       }
-    return (  <div className="s">
-      Enter Name  <input ref={val1} type="text" name="" id="" />
-      <button onClick={Add}>Set The Name</button>
-    </div> );
+        const token2 = useSelector((state) => state);
+
+    return (
+      <div className="s">
+        Enter Name <input ref={val1} type="text" name="" id="" />
+        <button onClick={Add}>Set The Name</button>
+        <Link to="/ck">GO to CK Page</Link>
+        
+        {token2.Data}
+      </div>
+    );
 }
  
 export default Sign;
